b889564182be35f6b326663d5741bbfccdc44db7
SergeiAntonenko <gerbar50@gmail.com>
2024-03-24 00:16:47 +0200

    upd const

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,38 +6,39 @@ import Feedback from "../Feedback/Feedback";
 import Notification from "../Notification/Notification";
 
 const App = () => {
-  const [options, setOptions] = useState(() => {
-    const localStorageData = window.localStorage.getItem("options");
+  const [countFeedbacks, setcountFeedbacks] = useState(() => {
+    const localStorageData = window.localStorage.getItem("feedbacks");
     return localStorageData
       ? JSON.parse(localStorageData)
       : { good: 0, neutral: 0, bad: 0 };
   });
-  const totalFeedback = options.good + options.neutral + options.bad;
+  const totalFeedback =
+    countFeedbacks.good + countFeedbacks.neutral + countFeedbacks.bad;
   const positiveFeedback =
-    Math.round((options.good / totalFeedback) * 100) || 0;
+    Math.round((countFeedbacks.good / totalFeedback) * 100) || 0;
 
   useEffect(() => {
-    window.localStorage.setItem("options", JSON.stringify(options));
-  }, [options]);
+    window.localStorage.setItem("feedbacks", JSON.stringify(countFeedbacks));
+  }, [countFeedbacks]);
 
   const updateFeedback = (feedbackType) => {
     if (feedbackType === "good") {
-      setOptions({
-        ...options,
-        good: options.good + 1,
+      setcountFeedbacks({
+        ...countFeedbacks,
+        good: countFeedbacks.good + 1,
       });
     } else if (feedbackType === "bad") {
-      setOptions({
-        ...options,
-        bad: options.bad + 1,
+      setcountFeedbacks({
+        ...countFeedbacks,
+        bad: countFeedbacks.bad + 1,
       });
     } else if (feedbackType === "neutral") {
-      setOptions({
-        ...options,
-        neutral: options.neutral + 1,
+      setcountFeedbacks({
+        ...countFeedbacks,
+        neutral: countFeedbacks.neutral + 1,
       });
     } else if (feedbackType === "reset") {
-      setOptions({
+      setcountFeedbacks({
         good: 0,
         neutral: 0,
         bad: 0,
@@ -51,9 +52,9 @@ const App = () => {
       <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
       {totalFeedback > 0 && (
         <Feedback
-          good={options.good}
-          neutral={options.neutral}
-          bad={options.bad}
+          good={countFeedbacks.good}
+          neutral={countFeedbacks.neutral}
+          bad={countFeedbacks.bad}
           totalFeedback={totalFeedback}
           positiveFeedback={positiveFeedback}
         />
